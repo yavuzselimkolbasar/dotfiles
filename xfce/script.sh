5c1ae138a63071f10dc897fd16538a89b664999c
Yavuz Selim <112783581+yavuzselimkolbasar@users.noreply.github.com>
2025-08-22 15:40:56 +0000

    Update script.sh

--- xfce/script.sh
+++ xfce/script.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-sudo mv /etc/nixos/hardware-configuration.nix ~/dotfiles/xfce/
-sudo mv /etc/nixos/configuration.nix ~/dotfiles/xfce/
-
-sudo cat ~/dotfiles/xfce/hardware-configuration.nix | sudo tee -a ~/dotfiles/xfce/hardware-configuration (1).nix
-
 sudo cp -r ~/dotfiles/xfce/configuration (1).nix /etc/nixos/
 sudo cp -r ~/dotfiles/xfce/hardware-configuration (1).nix /etc/nixos/
 sudo cp -r ~/dotfiles/xfce/network (1).nix /etc/nixos/
